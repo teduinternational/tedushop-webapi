b260d1cbdc083a19b6150458dbc2223afae42668
Toanbn <ngoctoan89@gmail.com>
2017-05-20 10:32:41 +0700

    fix bug get function parent

--- TeduShop.Data/Repositories/FunctionRepository.cs
+++ TeduShop.Data/Repositories/FunctionRepository.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using TeduShop.Data.Infrastructure;
 using TeduShop.Model.Models;
-using System.Linq;
 
 namespace TeduShop.Data.Repositories
 {
@@ -24,13 +23,16 @@ namespace TeduShop.Data.Repositories
         /// <returns></returns>
         public List<Function> GetListFunctionWithPermission(string userId)
         {
-            var query = from f in DbContext.Functions
-                        join p in DbContext.Permissions on f.ID equals p.FunctionId
-                        join r in DbContext.AppRoles on p.RoleId equals r.Id
-                        join ur in DbContext.UserRoles on r.Id equals ur.RoleId
-                        join u in DbContext.Users on ur.UserId equals u.Id
-                        where u.Id == userId && (p.CanRead == true)
-                        select f;
+            var query = (from f in DbContext.Functions
+                         join p in DbContext.Permissions on f.ID equals p.FunctionId
+                         join r in DbContext.AppRoles on p.RoleId equals r.Id
+                         join ur in DbContext.UserRoles on r.Id equals ur.RoleId
+                         join u in DbContext.Users on ur.UserId equals u.Id
+                         where u.Id == userId && (p.CanRead == true)
+                         select f);
+            var parentIds = query.Select(x => x.ParentId).Distinct();
+            query = query.Union(DbContext.Functions.Where(f => parentIds.Contains(f.ID)));
+
             return query.ToList();
         }
     }
